098a0bc991fdee4929b35dd137ab178114c5b338
Niklas Jörg STADLER <wi19b065@technikum-wien.at>
2021-03-22 19:16:02 +0100

    Add project files.

---
+++ DelegateExample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DelegateExample
+{
+    class Program
+    {
+        private delegate int Rechner(int zahl1, int zahl2);
+
+        static void Main(string[] args)
+        {
+            Rechner rechner;
+
+            rechner = new Rechner(Addiere);
+            int ergebnis = rechner(3, 2);
+
+            Console.WriteLine("Ergebnis der Addition:" + ergebnis); // 5
+
+            rechner = new Rechner(Subtrahiere);
+            ergebnis = rechner(3, 2);
+
+            Console.WriteLine("Ergebnis der Subtraktion:" + ergebnis); // 1
+
+            Console.ReadKey();
+
+
+        }
+
+
+        private static int Addiere(int zahl1, int zahl2)
+        { return zahl1 + zahl2; }
+
+        private static int Subtrahiere(int zahl1, int zahl2)
+        { return zahl1 - zahl2; }
+    }
+}
